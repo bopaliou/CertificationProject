785d806528ce6cce799ab50fbef0a5f2ddb5c590
Aliou Bop <bopaliou1994@gmail.com>
2021-08-08 10:57:59 +0200

    modification de notre templates

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\DocumentRepository;
+use App\Repository\ImageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function index(): Response
+    public function index(DocumentRepository $documentRepository,ImageRepository $imageRepository): Response
     {
+        $documents=$documentRepository->findAll();
+        $images=$imageRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'documents' => $documents,
+            'images' => $images,
         ]);
     }
 }
